fb84bebeb18b0601262219fcddf76ae3c474c613
Karthik <105837783+rtik007@users.noreply.github.com>
2025-08-22 20:10:33 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-doc_id = check[~check["match"]].index[0]   # pick the first mismatch
-print("Original doc_text:")
-print(check.loc[doc_id, "_doc_text"][:500])
 
-print("\nRebuilt from page_text:")
-print(check.loc[doc_id, "_joined_from_pages"][:500])
